b1e47b92e9cce10793db9cf34769beb50ac5b2d5
MarcosPorta <marcosporta15@gmail.com>
2022-02-10 12:16:06 -0300

    Activity's y pasajes.

--- app/src/main/java/com/marcosporta/ligasfofutbol/MainActivity.kt
+++ app/src/main/java/com/marcosporta/ligasfofutbol/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.marcosporta.ligasfofutbol
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickPartidos(view: View){
+        var intent= Intent(this,PartidosActivity::class.java)
+        startActivity(intent)
+        //toast
+        Toast.makeText(this,"Fixture", Toast.LENGTH_LONG).show()
+    }
+    fun clickPosiciones(view: View){
+        var intent= Intent(this,PosicionesActivity::class.java)
+        startActivity(intent)
+        //toast
+        Toast.makeText(this,"Posiciones",Toast.LENGTH_LONG).show()
     }
 }
---
+++ app/src/main/java/com/marcosporta/ligasfofutbol/PartidosActivity.kt
@@ -0,0 +1,11 @@
+package com.marcosporta.ligasfofutbol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PartidosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_partidos)
+    }
+}
