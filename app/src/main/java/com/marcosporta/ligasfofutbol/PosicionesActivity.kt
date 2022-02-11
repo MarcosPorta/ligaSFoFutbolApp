fafff435871bf92dfea31190579d9fb89f58cb94
MarcosPorta <marcosporta15@gmail.com>
2022-02-11 12:43:59 -0300

    Estructura PosicionesActivity.kt

--- app/src/main/java/com/marcosporta/ligasfofutbol/PosicionesActivity.kt
+++ app/src/main/java/com/marcosporta/ligasfofutbol/PosicionesActivity.kt
@@ -2,10 +2,35 @@ package com.marcosporta.ligasfofutbol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class PosicionesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posiciones)
+
+        val spinner: Spinner = findViewById(R.id.sp_categoriaPos)
+
+        ArrayAdapter.createFromResource(this,R.array.categorias,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+        val spinner2: Spinner = findViewById(R.id.sp_zonaPos)
+
+        ArrayAdapter.createFromResource(this,R.array.zonas,
+        android.R.layout.simple_spinner_dropdown_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner2.adapter = adapter
+        }
     }
 }
