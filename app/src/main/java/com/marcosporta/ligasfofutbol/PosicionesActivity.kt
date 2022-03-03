fcff6798d4c203cfd51ecca309f081276dc6b9cf
MarcosPorta <marcosporta15@gmail.com>
2022-03-03 17:41:50 -0300

    Spinners en posiciones, mejora en la logica.

--- app/src/main/java/com/marcosporta/ligasfofutbol/PosicionesActivity.kt
+++ app/src/main/java/com/marcosporta/ligasfofutbol/PosicionesActivity.kt
@@ -2,40 +2,108 @@ package com.marcosporta.ligasfofutbol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.view.View
+import android.widget.*
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
+import java.util.*
 
 class PosicionesActivity : AppCompatActivity() {
+
+    lateinit var mAdView : AdView
+    val testId= Arrays.asList("572A1A67BA6623DBD9D945D4043174CB")
+    val configuracion= RequestConfiguration.Builder().setTestDeviceIds(testId).build()
+    var tbPosiciones: TableLayout?=null
+    lateinit var spinnerZona:Spinner
+    lateinit var spinnerCat:Spinner
+    lateinit var spinnerTor:Spinner
+    var zonaSeleccionada:String = "Zona"
+    var categoriaSeleccionada:String = "Categoria"
+    var torneoSeleccionado:String = "Torneo"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posiciones)
+
+        MobileAds.setRequestConfiguration(configuracion)
+        RequestConfiguration.Builder().setTestDeviceIds(testId)
+        MobileAds.initialize(this) {}
+
+        //Banner
+        mAdView = findViewById(R.id.bannerPosiciones)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
 
         //Poner boton regresar y titulo en el Action Bar
         supportActionBar?.title = "Posiciones"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val spinner: Spinner = findViewById(R.id.sp_categoriaPos)
+        //SPINNERS
+            //Zona
+        spinnerZona = findViewById(R.id.sp_zonaPos)
+        val listaZona = resources.getStringArray(R.array.zonas)
 
-        ArrayAdapter.createFromResource(this,R.array.categorias,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            spinner.adapter = adapter
+        val adaptadorZona = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,listaZona)
+        spinnerZona.adapter = adaptadorZona
+
+        spinnerZona.onItemSelectedListener = object:
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+                zonaSeleccionada = spinnerZona.selectedItem.toString()
+
+                //Funcionalidad para realizar las consultas.
+                consultasPosiciones(zonaSeleccionada,categoriaSeleccionada,torneoSeleccionado)
+            }
+            //Cuando NO tengo un elemento seleccionado.
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+            //Categoria
+        spinnerCat = findViewById(R.id.sp_categoriaPos)
+        val listaCat = resources.getStringArray(R.array.categorias)
+
+        val adaptadorCat = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,listaCat)
+        spinnerCat.adapter = adaptadorCat
+
+        spinnerCat.onItemSelectedListener = object:
+            AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                categoriaSeleccionada = spinnerCat.selectedItem.toString()
+                //Funcionalidad para realizar las consultas.
+                consultasPosiciones(zonaSeleccionada,categoriaSeleccionada,torneoSeleccionado)
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+        spinnerTor = findViewById(R.id.sp_torneoPos)
+        val listaTor = resources.getStringArray(R.array.torneos)
+
+        val adaptadorTor = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,listaTor)
+        spinnerTor.adapter = adaptadorTor
+
+        spinnerTor.onItemSelectedListener = object:
+            AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                torneoSeleccionado = spinnerTor.selectedItem.toString()
+                //Funcionalidad para realizar las consultas.
+                consultasPosiciones(zonaSeleccionada,categoriaSeleccionada,torneoSeleccionado)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
         }
 
-        val spinner2: Spinner = findViewById(R.id.sp_zonaPos)
-
-        ArrayAdapter.createFromResource(this,R.array.zonas,
-        android.R.layout.simple_spinner_dropdown_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            spinner2.adapter = adapter
         }
 
+        private fun consultasPosiciones(zona: String, categoria:String, torneo:String){
+            if (torneoSeleccionado != "Torneo" && zonaSeleccionada != "Zona" && categoriaSeleccionada != "Categoria"){
+                var url = "https://marcosporta.site/ligasfcoapp/pos$zona$categoria$torneo.php"
+                Toast.makeText(this,"url recibida: $url", Toast.LENGTH_LONG).show()
+                tbPosiciones=findViewById(R.id.tbPosiciones)
+                tbPosiciones?.removeAllViews()
+            }
 
-    }
+            }
 }
