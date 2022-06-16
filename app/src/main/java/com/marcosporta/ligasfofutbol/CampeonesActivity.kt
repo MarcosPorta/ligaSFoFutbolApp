bf9a6c4ae5ae6608e00ccf944794e28d5fb54c86
MarcosPorta <marcosporta15@gmail.com>
2022-06-16 16:13:52 -0300

    Comienzo de seccion CampeonesActivity.kt

--- app/src/main/java/com/marcosporta/ligasfofutbol/CampeonesActivity.kt
+++ app/src/main/java/com/marcosporta/ligasfofutbol/CampeonesActivity.kt
@@ -2,13 +2,17 @@ package com.marcosporta.ligasfofutbol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TableLayout
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.*
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.RequestConfiguration
+import org.json.JSONException
 import java.util.*
 
 class CampeonesActivity : AppCompatActivity() {
@@ -19,6 +23,8 @@ class CampeonesActivity : AppCompatActivity() {
     var tbCampeones: TableLayout?=null
     lateinit var spinnerCampeones1: Spinner
     lateinit var spinnerCampeones2: Spinner
+    var zonaSeleccionada:String = "Zona"
+    var moiSeleccionada:String = "Mayores"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,5 +53,68 @@ class CampeonesActivity : AppCompatActivity() {
         val listaCamp2 = resources.getStringArray(R.array.mayinf)
         val adaptadorCamp2 = ArrayAdapter(this,R.layout.spinner_style,listaCamp2)
         spinnerCampeones2.adapter = adaptadorCamp2
+
+        //Funcionalidad a los spinners
+        spinnerCampeones1.onItemSelectedListener = object:
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                zonaSeleccionada = spinnerCampeones1.selectedItem.toString()
+                cunsultasCampeones(zonaSeleccionada,moiSeleccionada)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+            }
+
+        spinnerCampeones2.onItemSelectedListener = object:
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                moiSeleccionada = spinnerCampeones2.selectedItem.toString()
+                cunsultasCampeones(zonaSeleccionada,moiSeleccionada)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+    }
+
+    private fun cunsultasCampeones(zona: String, categoria:String){
+        //Ver los casos
+        if (zonaSeleccionada!="Zona"){
+            var url = "https://marcosporta.site/ligasfcoapp/camp$zona$categoria.php"
+            Toast.makeText(this,url, Toast.LENGTH_LONG).show()
+
+            tbCampeones=findViewById(R.id.tbCampeones)
+            tbCampeones?.removeAllViews()
+
+            val queue= Volley.newRequestQueue(this)
+
+            val jsonObjectRequest= JsonObjectRequest(
+                Request.Method.GET,url,null,
+                { response ->
+                    try {
+                        val jsonArray = response.getJSONArray("data")
+                        for(i in 0 until jsonArray.length() ) {
+                            val jsonObject = jsonArray.getJSONObject(i)
+                            val registro=LayoutInflater.from(this).inflate(R.layout.text_titulo,null,false)
+                            val registro2=LayoutInflater.from(this).inflate(R.layout.table_row_campeones,null,false)
+                            val tituloCamp=registro.findViewById<View>(R.id.tituloCamp) as TextView
+                            val campeon=registro2.findViewById<View>(R.id.colEquipoCamp) as TextView
+                            val fechaTitulo=registro2.findViewById<View>(R.id.colFechaTit) as TextView
+                            tituloCamp.text=jsonObject.getString("titulo")
+                            campeon.text=jsonObject.getString("equipo")
+                            fechaTitulo.text=jsonObject.getString("fecha")
+                            tbCampeones?.addView(registro)
+                            tbCampeones?.addView(registro2)
+                        }
+                    }catch (e: JSONException){
+                        e.printStackTrace()
+                    }
+                }, { error ->
+                    Toast.makeText(this,"Error $error ", Toast.LENGTH_LONG).show()
+                }
+            )
+            queue.add(jsonObjectRequest)
+        }
     }
 }
